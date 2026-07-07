5ec82438caa6d6e3e190bf1080558da2cd82e2ed
Thiago Kosovski <thikosovski@gmail.com>
2026-07-07 16:43:43 -0300

    Configura ALLOWED_HOSTS para Render 1

--- config/settings.py
+++ config/settings.py
@@ -28,11 +28,10 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "simulabank.onrender.com",
-]
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="127.0.0.1,localhost",
+).split(",")
 
 
 # Application definition
